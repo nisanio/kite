b7b8f7a82e1399f3d1e951ea430d42f4be73fa4f
Alejandro Caballero Salas <caballerosalas@gmail.com>
2026-02-13 11:34:39 -0300

    Brief test to check if and, or, and not are tokenized as real keywords

--- src/main.c
+++ src/main.c
@@ -5,21 +5,16 @@
 #include <stdio.h>
 
 int main(void) {
-    const char *source =
-        "1 + 2 < 5\n"
-        "1 + 2 == 4\n";
+    const char *source = "and or not";
 
     Lexer lexer;
     lexer_init(&lexer, source);
 
-    Parser parser;
-    parser_init(&parser, &lexer);
-
-    Program *program = parse_program(&parser);
-
-    Env *global = env_create(NULL);
-
-    eval_program(program, global);
+    Token tok;
+    do {
+        tok = lexer_next(&lexer);
+        printf("Token type: %d\n", tok.type);
+    } while (tok.type != TOK_EOF);
 
     return 0;
 }
